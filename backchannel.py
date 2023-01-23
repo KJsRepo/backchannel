ff664cedaeca1df8264679c73a9cee4e5be823f9
Keith Jones <kjsothermail+kjsrepo@gmail.com>
2023-01-23 17:42:11 -0600

    'Added my own slow typing function'

--- backchannel.py
+++ backchannel.py
@@ -1,21 +1,22 @@
 #!/bin/python3
 import json
+import sys
 import websocket
 from time import time
 from time import sleep
 from datetime import datetime
 
 def on_message(ws, message):
-    print('&&&&&&&&&& MESSAGE!')
-    print("v")
-    print(message)
+    #print('&&&&&&&&&& MESSAGE!')
+    #print("v")
+    #print(message)
     data = json.loads(message)
     if 0 <= 2 < len(data):
         print("*****")
         print("* dt:'" + str(datetime.fromtimestamp(data[2]['created_at'])) + "'       " + data[2]['pubkey'] + ":")
         print("* MSG:")
         output(data[2]['content'])
-        print('***')
+        print("\n***")
         print("\n\n")
     else:
         print("--------------NOPE")
@@ -28,14 +29,16 @@ def on_close(ws):
     print("Connection closed")
 
 def on_open(ws):
-    data = '["REQ", "ec3e6228-5ef3-4162-899c-a58e88de586a", {"kinds":[1], "since":' + str(round((time() - 1000))) + '}]'
+    #data = '["REQ", "ec3e6228-5ef3-4162-899c-a58e88de586a", {"kinds":[1], "since":' + str(round((time() - 1000))) + '}]'
+    data = '["REQ", "ec3e6228-5ef3-4162-899c-a58e88de586a", {"kinds":[1], "limit":4}]'
     json_data = json.dumps(data)
     ws.send(data)
 
 def output(outStr):                                                                                                                                                                                                                                      
-    for a in outStr[0:len(outStr) - 2]:                                                                                                                                                                                                                                     
-        print(a, end='')                                                                                                                                                                                                                                 
-        sleep(0.1)  
+    for a in outStr:                                                                                                                                                                                                                                     
+        print(a,end='')                                                                                                                                                                                                                                 
+        sleep(0.01)
+        sys.stdout.flush()
 
 if __name__ == "__main__":
     #websocket.enableTrace(True)
